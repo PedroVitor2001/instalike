c156db94924895c7d4f82ea5d434c07a6d593bb7
Pedro Petrilli <ppetrilli96@gmail.com>
2024-11-19 11:27:40 -0300

    feat: create post search by id

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 import express from "express";
 
+const posts = [
+  {
+    id: 1,
+    descricao: "Foto teste",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 2,
+    descricao: "Gato curioso",
+    imagem: "https://placekitten.com/200/300",
+  },
+  {
+    id: 3,
+    descricao: "Gatinho dormindo",
+    imagem: "https://placekitten.com/300/200",
+  },
+];
+
 const app = express();
+app.use(express.json());
+
+function searchPostById(id) {
+  return posts.findIndex((post) => {
+    return post.id === Number(id);
+  });
+}
+
 app.listen(3000, () => {
   console.log("Servidor ligado...");
 });
 
-app.get("/api", (req, res) => {
-  res.status(200).send("Ok");
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
+});
+
+app.get("/posts/:id", (req, res) => {
+  const index = searchPostById(req.params.id);
+  res.status(200).json(posts[index]);
 });
