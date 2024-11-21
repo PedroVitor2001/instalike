ac2c7724e7662ed61ff69bb87d6ae2d465978441
Pedro Petrilli <ppetrilli96@gmail.com>
2024-11-21 12:07:23 -0300

    feat: upload image function

--- src/controller/posts-controller.js
+++ src/controller/posts-controller.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import { createPost, getPosts } from "../models/posts-model.js";
 
 //GET
@@ -11,6 +12,23 @@ export async function createNewPost(req, res) {
   const newPost = req.body;
   try {
     const createdPost = await createPost(newPost);
+    res.status(200).json(createdPost);
+  } catch (err) {
+    console.error("Error when creating post", err.message);
+    res.status(500).json({ "erro?": "Requisition failed" });
+  }
+}
+
+export async function uploadNewImage(req, res) {
+  const newImage = {
+    description: "",
+    imgUrl: req.file.originalname,
+    alt: "",
+  };
+  try {
+    const createdPost = await createPost(newImage);
+    const updatedImg = `uploads/${createdPost.insertedId}.png`;
+    fs.renameSync(req.file.path, updatedImg);
     res.status(200).json(createdPost);
   } catch (err) {
     console.error("Error when creating post", err.message);
--- src/routes/post-routes.js
+++ src/routes/post-routes.js
@@ -1,11 +1,28 @@
+import multer from "multer";
 import express from "express";
-import { createNewPost, postsList } from "../controller/posts-controller.js";
+import {
+  createNewPost,
+  postsList,
+  uploadNewImage,
+} from "../controller/posts-controller.js";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
   app.use(express.json());
 
   app.get("/posts", postsList);
   app.post("/posts", createNewPost);
+  app.post("/upload", upload.single("image"), uploadNewImage);
 };
 
 export default routes;
